05d7846f20ce63ec036a6de5b67da5c063b0aa31
heroyan <zndxysf@126.com>
2019-05-20 20:54:43 +0800

    生成index html文件脚本添加

---
+++ genIndexHtml.py
@@ -0,0 +1,34 @@
+#coding:utf8
+import os
+
+def genIndexHtml(rootDir, name):
+    with open('%s/index.html' % rootDir, 'w') as index:
+        index.write('<title>%s</title>' % name)
+        index.write('<script>function alterTitle(title){document.title=title;}</script><style>.main{width:100%;height: 100%;} .side {float:left;max-width:200px;height:100%;overflow:scroll;border:1px solid;} .content {overflow:hidden;} </style>\n')
+        index.write('<div class="main"><div class="side">')
+        for root, dirs, files in os.walk(rootDir):
+            for f in files:
+                if f.endswith('.html') and f != 'index.html':
+                    index.write('<h6><a onclick="alterTitle(\'%s\')" target="main" href="%s">%s</a></h6>\n' % (f, f, f))
+
+        index.write('</div><div class="content"><iframe width=100% height=100% name="main"/></div></div>')
+
+def main():
+    filters = ['.git', '.vscode']
+    for root, dirs, files in os.walk('.'):
+        for d in dirs:
+            dir = os.path.join(root, d)
+            flag = False
+            for f in filters:
+                if f in dir:
+                    flag = True
+                    break
+
+            if flag:
+                continue
+
+            print dir
+            genIndexHtml(os.path.join(root, d), d)
+
+if __name__ == '__main__':
+    main()
